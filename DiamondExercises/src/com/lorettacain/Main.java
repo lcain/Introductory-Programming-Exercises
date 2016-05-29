d89cb2d3b195b66bf2bf0b2f53b7d93b95fdafa3
Loretta Cain <locacodes@gmail.com>
2016-05-29 17:01:38 +1000

    diamond

--- DiamondExercises/src/com/lorettacain/Main.java
+++ DiamondExercises/src/com/lorettacain/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	    isosceles(5);
+        diamond(5);
     }
 
     public static void isosceles(int lines){
@@ -11,7 +12,7 @@ public class Main {
         String space = "";
         System.out.println("This is an isosceles triangle:");
 
-        for (int i = 0; i < lines; i++){
+        for (int i = 1; i < lines; i++){
             space += " ";
         }
 
@@ -19,6 +20,29 @@ public class Main {
             space = space.substring(0, space.length()-1);
             System.out.println(space + asterisk);
             asterisk += "**";
+        }
+
+    }
+
+    public static void diamond(int lines) {
+
+        String asterisk = "*";
+        String space = "";
+        System.out.println("This is a diamond:");
+
+        for (int i = 1; i < lines; i++){
+            space += " ";
+        }
+
+        for (int i = 1; i < lines; i++) {
+            space = space.substring(0, space.length()-1);
+            System.out.println(space + asterisk);
+            asterisk += "**";
+        }
+        for (int i = 1; i < lines; i++) {
+            asterisk = asterisk.substring(0, asterisk.length()-2);
+            System.out.println(space + asterisk);
+            space += " ";
         }
 
     }
