ccfb6524ccd1b2f31d37968c1bc0ef5fd44ea3a0
Loretta Cain <locacodes@gmail.com>
2016-05-30 00:17:04 +1000

    Diamonds done

--- DiamondExercises/src/com/lorettacain/Main.java
+++ DiamondExercises/src/com/lorettacain/Main.java
@@ -4,7 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	    isosceles(5);
-        diamond(5);
+        diamond(6);
+        namedDiamond(5, "Loretta");
     }
 
     public static void isosceles(int lines){
@@ -26,24 +27,58 @@ public class Main {
 
     public static void diamond(int lines) {
 
-        String asterisk = "*";
-        String space = "";
+        String asterisk1 = "*";
+        String space1 = "";
         System.out.println("This is a diamond:");
 
-        for (int i = 1; i < lines; i++){
-            space += " ";
+        for (int i = 0; i < lines; i++){
+            space1 += " ";
         }
 
-        for (int i = 1; i < lines; i++) {
-            space = space.substring(0, space.length()-1);
-            System.out.println(space + asterisk);
-            asterisk += "**";
+        for (int i = 0; i < lines; i++) {
+            space1 = space1.substring(0, space1.length()-1);
+            System.out.println(space1 + asterisk1);
+            asterisk1 += "**";
         }
         for (int i = 1; i < lines; i++) {
-            asterisk = asterisk.substring(0, asterisk.length()-2);
-            System.out.println(space + asterisk);
-            space += " ";
+            if(i == 1 ) {
+                asterisk1 = asterisk1.substring(0, asterisk1.length() - 2);
+                space1 += " ";
+            }
+            asterisk1 = asterisk1.substring(0, asterisk1.length()-2);
+            System.out.println(space1 + asterisk1);
+            space1 += " ";
         }
 
+    }
+
+    public static void namedDiamond(int lines, String name){
+        String asterisk1 = "*";
+        String space1 = "";
+        System.out.println("This is a named diamond:");
+
+        for (int i = 0; i < lines; i++){
+            space1 += " ";
+        }
+
+        for (int i = 1; i <= lines; i++) {
+            if (i == lines){
+                System.out.println(name);
+            }
+            else {
+
+                space1 = space1.substring(0, space1.length() - 1);
+                System.out.println(space1 + asterisk1);
+                asterisk1 += "**";
+
+            }
+
+        }
+        for (int i = 1; i < lines; i++) {
+
+            asterisk1 = asterisk1.substring(0, asterisk1.length()-2);
+            System.out.println(space1 + asterisk1);
+            space1 += " ";
+        }
     }
 }
