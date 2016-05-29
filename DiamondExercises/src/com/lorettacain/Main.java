1f40960eae9fe0a648bce6cfdc5b20f754b81d3f
Loretta Cain <locacodes@gmail.com>
2016-05-29 16:53:03 +1000

    isosceles

--- DiamondExercises/src/com/lorettacain/Main.java
+++ DiamondExercises/src/com/lorettacain/Main.java
@@ -3,10 +3,23 @@ package com.lorettacain;
 public class Main {
 
     public static void main(String[] args) {
-	    rightTriangle();
+	    isosceles(5);
     }
 
-    public static void rightTriangle(){
+    public static void isosceles(int lines){
+        String asterisk = "*";
+        String space = "";
+        System.out.println("This is an isosceles triangle:");
+
+        for (int i = 0; i < lines; i++){
+            space += " ";
+        }
+
+        for (int i = 1; i < lines; i++) {
+            space = space.substring(0, space.length()-1);
+            System.out.println(space + asterisk);
+            asterisk += "**";
+        }
 
     }
 }
