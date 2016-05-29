ba0014e467c43b1773f9da3ab98b6277a290be6f
Loretta Cain <locacodes@gmail.com>
2016-05-29 14:39:37 +1000

    Triangles done

--- TriangleExcercises/src/com/lorettacain/Main.java
+++ TriangleExcercises/src/com/lorettacain/Main.java
@@ -3,6 +3,48 @@ package com.lorettacain;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        singleAsterisk();
+        vertical(4);
+        horizontal(4);
+        rightTriangle(4);
+
     }
+
+    public static String singleAsterisk(){
+        String asterisk = "*";
+        System.out.println("This is a single asterisk:");
+        System.out.println(asterisk);
+        return asterisk;
+    }
+
+    public static void vertical(int number){
+        String asterisk = "*";
+        System.out.println("This is a vertical line:");
+        for (int i = 0; i < number; i++) {
+            System.out.println(asterisk);
+        }
+    }
+
+    public static void horizontal(int number){
+        String asterisk = "";
+        System.out.println("This is a horizontal line:");
+        for (int i = 0; i < number; i++) {
+            asterisk += "*";
+        }
+        System.out.println(asterisk);
+
+    }
+
+    public static void rightTriangle(int number){
+        String asterisk = "";
+        System.out.println("This is a right triangle:");
+        for (int i = 0; i < number; i++) {
+            asterisk += "*";
+            System.out.println(asterisk);
+        }
+
+    }
+
+
 }
